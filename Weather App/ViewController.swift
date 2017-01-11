07ea04e00502b24ff8cb2b3b8593bf056bcf823b
lizhealy <lizhealy8@gmail.com>
2017-01-11 15:13:44 -0500

    weather app commit

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -9,6 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var weatherText: UITextField!
+    
+    @IBOutlet weak var weatherUpdate: UILabel!
+    
+    @IBAction func searchButton(_ sender: Any) {
+        
+        let theCity = weatherText.text
+        
+        let city = theCity?.replacingOccurrences(of: " ", with: "-")
+        
+        if city == "" {
+            weatherUpdate.text = "Please enter a city"
+        }
+        
+        else {
+            
+            var message = ""
+        
+            let cityUrl = "http://www.weather-forecast.com/locations/" + city! + "/forecasts/latest"
+        
+            if let url = URL(string: cityUrl) {
+                
+                let request = NSMutableURLRequest(url: url)
+                
+                let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                    data, response, error in
+                    
+                    if error != nil {
+                        print(error)
+                    }
+                    else {
+                        if let unwrappedData = data {
+                            let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                            
+                            let intro = "3 Day Weather Forecast Summary"
+                            if (dataString?.contains(intro))! {
+                                
+                                if let contentArray = dataString?.components(separatedBy: "3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">") {
+                                    //print(contentArray[1])
+                                    
+                                    if contentArray.count > 1 {
+                                        let newContent = contentArray[1].components(separatedBy: "</")
+                                        
+                                        if newContent.count > 1 {
+                                            
+                                            message = newContent[0].replacingOccurrences(of: "&deg;", with: "°")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if message == "" {
+                        message = "The Weather could not be found, please try again"
+                    }
+                    DispatchQueue.main.sync(execute: {
+                        //UpdateUI
+                        self.weatherUpdate.text = message
+                    })
+                    
+                }
+            task.resume()
+            
+            }
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
